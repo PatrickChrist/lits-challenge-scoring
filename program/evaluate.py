8b1ee00372ad7e786bc2f84ad4364ec1da3fe378
Patrick Christ <patrick.christ@cdtm.de>
2017-02-01 14:27:08 +0100

    First local test milestone success

--- program/evaluate.py
+++ program/evaluate.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import sys, os, os.path
+import nibabel as nb
+import glob
+import helpers.calc_metric
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
@@ -17,13 +20,37 @@ if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
     output_filename = os.path.join(output_dir, 'scores.txt')              
     output_file = open(output_filename, 'wb')
 
-    gold_list = os.listdir(truth_dir)
-    for gold in gold_list:
-    	gold_file = os.path.join(truth_dir, gold)
-    	corresponding_submission_file = os.path.join(submit_dir, gold)
-    	if os.path.exists(corresponding_submission_file):
-    		pi = float(open(gold_file, 'rb').read())
-    		guess = float(open(corresponding_submission_file, 'rb').read())
-    		diff = abs(pi - guess)
-    		output_file.write("Difference: %f" % diff)
+    reference_vol_list = glob.glob(truth_dir + '/*.nii')
+    print reference_vol_list
+    for reference_vol in reference_vol_list:
+
+		print 'Starting with volume %s' % reference_vol
+
+		reference_volume_path = reference_vol
+		submission_volume_path = os.path.join(submit_dir, os.path.basename(reference_vol))
+		if os.path.exists(submission_volume_path):
+			print 'Found corresponding submission file %s for reference file %s' % (reference_volume_path,submission_volume_path)
+
+			# Load Ref and submission with Nibabel
+			loaded_reference_volume=nb.load(reference_volume_path)
+			loaded_submission_volume = nb.load(submission_volume_path)
+
+			# Get the current voxelspacing
+			reference_voxelspacing= loaded_reference_volume.header.get_zooms()[:3]
+
+			# Get Numpy data
+			loaded_reference_volume_data = loaded_reference_volume.get_data()
+			loaded_submission_volume_data = loaded_submission_volume.get_data()
+
+			# Calc metric and store them in dict
+			print 'Start calculating metrics for submission file %s' % submission_volume_path
+			current_result = helpers.calc_metric.get_scores(loaded_submission_volume_data==2,loaded_reference_volume_data==2,reference_voxelspacing)
+
+			print 'Found following results for submission file %s: %s' % (submission_volume_path,current_result)
+			output_file.write("ISBIDiceComplete: %f\n" % current_result['dice'])
+			output_file.write("ISBIVOEComplete: %f\n" % current_result['voe'])
+			output_file.write("ISBIRVDComplete: %f\n" % current_result['rvd'])
+			output_file.write("ISBIASSDComplete: %f\n" % current_result['assd'])
+			output_file.write("ISBIMSSDComplete: %f\n" % current_result['msd'])
+
     output_file.close()
---
+++ program/helpers/calc_metric.py
@@ -0,0 +1,37 @@
+from medpy import metric
+from surface import Surface
+import numpy as np
+
+
+
+def get_scores(pred,label,vxlspacing):
+    '''
+    Calculates metrics scores from numpy arrays and returns an dict.
+    :param pred: numpy.array
+    :param label: numpy.array
+    :param vxlspacing: list with x,y and z spacing
+    :return: dict with dice, jaccard, voew, rvd, assd, and msd
+    '''
+    # Test whether arrays have equal size
+    if pred.shape!=label.shape:
+        print 'Shapes to not match! Pred %s and Label %s' % (pred.shape,label.shape)
+        raise AttributeError
+
+
+    volscores = {}
+
+    volscores['dice'] = metric.dc(pred,label)
+    volscores['jaccard'] = metric.binary.jc(pred,label)
+    volscores['voe'] = 1. - volscores['jaccard']
+    volscores['rvd'] = metric.ravd(label,pred)
+
+    if np.count_nonzero(pred) ==0 or np.count_nonzero(label)==0:
+        volscores['assd'] = 0
+        volscores['msd'] = 0
+    else:
+        evalsurf = Surface(pred,label,physical_voxel_spacing = vxlspacing,mask_offset = [0.,0.,0.], reference_offset = [0.,0.,0.])
+        volscores['assd'] = evalsurf.get_average_symmetric_surface_distance()
+
+        volscores['msd'] = metric.hd(label,pred,voxelspacing=vxlspacing)
+
+    return volscores
