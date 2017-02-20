Change to Int8 and changed MSD calculation - Bugfix

program/evaluate.py
@@ -49,13 +49,15 @@ if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
 			# Get the current voxelspacing
 			reference_voxelspacing= loaded_reference_volume.header.get_zooms()[:3]
 
-			# Get Numpy data and compress to int9
-			loaded_reference_volume_data = loaded_reference_volume.get_data().astype(np.int8)
-			loaded_submission_volume_data =loaded_submission_volume.get_data().astype(np.int8)
+			# Get Numpy data and compress to int8
+			loaded_reference_volume_data = (loaded_reference_volume.get_data()).astype(np.int8)
+			loaded_submission_volume_data = (loaded_submission_volume.get_data()).astype(np.int8)
 
 
 			# Calc metric and store them in dict
 			print 'Start calculating metrics for submission file %s' % submission_volume_path
+
+			print 'Found %s Lesion Pixels in file %s' % (np.count_nonzero(loaded_submission_volume_data==2),submission_volume_path)
 			current_result = helpers.calc_metric.get_scores(loaded_submission_volume_data==2,loaded_reference_volume_data==2,reference_voxelspacing)
 
 			print 'Found following results for submission file %s: %s' % (submission_volume_path,current_result)