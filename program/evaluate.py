1086c8b29fcc536deae5d7f4414a42d104d07217
Patrick Christ <patrick.christ@cdtm.de>
2017-06-05 17:14:30 +0200

    Add new metrics for liver and tumorburden estimation - Untested

--- program/evaluate.py
+++ program/evaluate.py
@@ -26,12 +26,29 @@ if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
 
 	# Iterate over all volumes in the reference list
 
-	dice=[]
-	voe=[]
-	rvd=[]
-	assd=[]
-	mssd=[]
+	dice_liv=[]
+	voe_liv=[]
+	rvd_liv=[]
+	assd_liv=[]
+	mssd_liv=[]
+	precision_liv=[]
+	recall_liv =[]
 
+
+	dice_les = []
+	voe_les = []
+	rvd_les = []
+	assd_les = []
+	mssd_les = []
+
+	precision_les = []
+	recall_les = []
+	obj_tpr_les =[]
+	obj_fpr_les = []
+
+
+	tumor_burden_rmse = []
+	tumor_burden_max_error = []
 
 	for reference_vol in reference_vol_list:
 
@@ -67,41 +84,105 @@ if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
 				if num_lesion_in_submission ==0:
 					print 'No lesions in prediction, well done'
 
-					current_result['dice'] = 1.
-					current_result['voe'] = 0.
-					current_result['rvd'] = 0.
-					current_result['assd'] = 0.
-					current_result['msd'] = 0.
+					current_result_les['dice'] = 1.
+					current_result_les['voe'] = 0.
+					current_result_les['rvd'] = 0.
+					current_result_les['assd'] = 0.
+					current_result_les['msd'] = 0.
+
+					current_result_les['precision'] = 0.
+					current_result_les['recall'] = 0.
+					current_result_les['obj_tpr'] = 0.
+					current_result_les['obj_fpr'] = 0.
+
 
 				elif num_lesion_in_submission!=0:
 					loaded_reference_volume_data = np.zeros_like(loaded_reference_volume_data)
 					# Set one pixel to be a lesion pixel to avoid divding through zero
 					loaded_reference_volume_data[0,0,0] = 2
-					current_result = helpers.calc_metric.get_scores(loaded_submission_volume_data == 2,
+					current_result_les = helpers.calc_metric.get_scores(loaded_submission_volume_data == 2,
 																	loaded_reference_volume_data == 2,
 																	reference_voxelspacing)
 
 			else:
-				current_result = helpers.calc_metric.get_scores(loaded_submission_volume_data==2,loaded_reference_volume_data==2,reference_voxelspacing)
+				current_result_les = helpers.calc_metric.get_scores(loaded_submission_volume_data==2,loaded_reference_volume_data==2,reference_voxelspacing)
+
+			# Check whether liver exists in Reference
+
+			num_liver_in_submission = np.count_nonzero(loaded_submission_volume_data == 1)
+
+			# Calculate liver metrics
+			if num_liver_in_submission==0:
+				loaded_submission_volume_data[0,0,0] = 1
+				current_result_liv = helpers.calc_metric.get_scores(loaded_submission_volume_data == 1,
+																	loaded_reference_volume_data == 1,
+																	reference_voxelspacing)
+			else:
+				current_result_liv = helpers.calc_metric.get_scores(loaded_submission_volume_data==1,loaded_reference_volume_data==1,reference_voxelspacing)
+
+
+			# Calculate tumorburden
+			if num_liver_in_submission!=0 and num_lesion_in_reference!=0:
+				tumorburden_diff=helpers.calc_metric.get_tumorburden_metric(loaded_submission_volume_data,loaded_reference_volume_data)
+			else:
+				if num_lesion_in_submission!=0:
+					tumorburden_diff=1.0
+				tumorburden_diff=0.0
 
 			print 'Found following results for submission file %s: %s' % (submission_volume_path,current_result)
 
-			dice.append(current_result['dice'])
-			voe.append(current_result['voe'])
-			rvd.append(current_result['rvd'])
-			assd.append(current_result['assd'])
-			mssd.append(current_result['msd'])
+			# Saving the current results
+			## Results for lesion
 
-	output_file.write("ISBIDiceComplete: %.2f\n" % np.mean(dice))
-	output_file.write("ISBIVOEComplete: %.2f\n" % np.mean(voe))
-	output_file.write("ISBIRVDComplete: %.2f\n" % np.mean(rvd))
-	output_file.write("ISBIASSDComplete: %.2f\n" % np.mean(assd))
-	output_file.write("ISBIMSSDComplete: %.2f\n" % np.mean(mssd))
+			dice_les.append(current_result_les['dice'])
+			voe_les.append(current_result_les['voe'])
+			rvd_les.append(current_result_les['rvd'])
+			assd_les.append(current_result_les['assd'])
+			mssd_les.append(current_result_les['msd'])
 
-	output_file.write("LBDiceComplete: %.2f\n" % np.mean(dice))
-	output_file.write("LBVOEComplete: %.2f\n" % np.mean(voe))
-	output_file.write("LBRVDComplete: %.2f\n" % np.mean(rvd))
-	output_file.write("LBASSDComplete: %.2f\n" % np.mean(assd))
-	output_file.write("LBMSSDComplete: %.2f\n" % np.mean(mssd))
+			precision_les.append(current_result_les['precision'])
+			recall_les.append(current_result_les['recall'])
+			obj_tpr_les.append(current_result_les['obj_tpr'])
+			obj_fpr_les.append(current_result_les['obj_fpr'])
 
+			## Results for liver
+			dice_liv.append(current_result_liv['dice'])
+			voe_liv.append(current_result_liv['voe'])
+			rvd_liv.append(current_result_liv['rvd'])
+			assd_liv.append(current_result_liv['assd'])
+			mssd_liv.append(current_result_liv['msd'])
+
+			precision_liv.append(current_result_liv['precision'])
+			recall_liv.append(current_result_liv['recall'])
+
+			## Results
+			tumor_burden_rmse.append(np.abs(tumorburden_diff))
+			tumor_burden_max_error.append(tumorburden_diff)
+
+	# Writing Output
+	## Output for lesion
+	output_file.write("Dice_Lesion: %.2f\n" % np.mean(dice_les))
+	output_file.write("VOE_Lesion: %.2f\n" % np.mean(voe_les))
+	output_file.write("RVD_Lesion: %.2f\n" % np.mean(rvd_les))
+	output_file.write("ASSD_Lesion: %.2f\n" % np.mean(assd_les))
+	output_file.write("MSSD_Lesion: %.2f\n" % np.mean(mssd_les))
+
+	output_file.write("Precision_Lesion: %.2f\n" % np.mean(precision_les))
+	output_file.write("Recall_Lesion: %.2f\n" % np.mean(recall_les))
+	output_file.write("OBJ_TPR_Lesion: %.2f\n" % np.mean(obj_tpr_les))
+	output_file.write("OBJ_FPR_Lesion: %.2f\n" % np.mean(obj_fpr_les))
+
+	## Output for liver
+	output_file.write("Dice_Liver: %.2f\n" % np.mean(dice_liv))
+	output_file.write("VOE_Liver: %.2f\n" % np.mean(voe_liv))
+	output_file.write("RVD_Liver: %.2f\n" % np.mean(rvd_liv))
+	output_file.write("ASSD_Liver: %.2f\n" % np.mean(assd_liv))
+	output_file.write("MSSD_Liver: %.2f\n" % np.mean(mssd_liv))
+
+	output_file.write("Precision_Liver: %.2f\n" % np.mean(precision_liv))
+	output_file.write("Recall_Liver: %.2f\n" % np.mean(recall_liv))
+
+	## Output for tumorburden
+	output_file.write("RMSE_Tumorburden: %.2f\n" % np.mean(tumor_burden_rmse))
+	output_file.write("MAXERROR_Tumorburden: %.2f\n" % np.max(tumor_burden_max_error))
 	output_file.close()
--- program/helpers/calc_metric.py
+++ program/helpers/calc_metric.py
@@ -4,6 +4,27 @@ import numpy as np
 
 
 
+
+def get_tumorburden_metric(pred,label):
+    '''Calculates the tumorburden and evalutes the tumor burden metrics RMSE and max error
+    :param pred: numpy.array
+    :param label: numpy.array
+    :param vxlspacing: list with x,y and z spacing
+    :return: dict with RMSE and Max error
+    '''
+    tumorburden_label=calc_tumorburden(label)
+    tumorburden_pred = calc_tumorburden(pred)
+
+    tumorburden_diff = tumorburden_label - tumorburden_pred
+    return tumorburden_diff
+
+def calc_tumorburden(vol):
+    num_liv_pix=np.count_nonzero(vol==1)
+    num_les_pix=np.count_nonzero(vol==2)
+
+    tumorburden = np.divide(num_les_pix,num_liv_pix)
+    return tumorburden
+
 def get_scores(pred,label,vxlspacing):
     '''
     Calculates metrics scores from numpy arrays and returns an dict.
@@ -24,6 +45,10 @@ def get_scores(pred,label,vxlspacing):
     volscores['jaccard'] = metric.binary.jc(pred,label)
     volscores['voe'] = 1. - volscores['jaccard']
     volscores['rvd'] = metric.ravd(label,pred)
+    volscores['precision'] = metric.binary.precision(label,pred)
+    volscores['recall'] = metric.binary.recall(label,pred)
+    volscores['obj_tpr'] =metric.binary.obj_tpr(label,pred)
+    volscores['obj_fpr'] =metric.binary.obj_fpr(label,pred)
 
     if np.count_nonzero(pred) ==0 or np.count_nonzero(label)==0:
         volscores['assd'] = 0
