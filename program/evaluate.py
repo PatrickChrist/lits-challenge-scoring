Fixed tumor burden calculation: incorrect masks were passed; now handling the possibility of a missing liver prediction. Also removed manual memory management (del, gc).

program/evaluate.py
@@ -6,7 +6,6 @@ import nibabel as nb
 import numpy as np
 from scipy.ndimage.measurements import label as label_connected_components
 import glob
-import gc
 
 from helpers.calc_metric import (dice,
                                  detect_lesions,
@@ -92,11 +91,6 @@ for reference_volume_fn in reference_volume_list:
         lesion_detection_stats['FP']+=FP
         lesion_detection_stats['FN']+=FN
         
-        # Clear some memory
-        del reference_volume
-        del submission_volume
-        gc.collect()
-        
         # Compute segmentation scores for DETECTED lesions.
         import time
         print("DEBUG {}: Computing lesion scores.".format(time.time()))
@@ -139,8 +133,8 @@ for reference_volume_fn in reference_volume_list:
             
         # Compute tumor burden.
         print("DEBUG {}: Computing tumor burden.".format(time.time()))
-        tumor_burden = compute_tumor_burden(prediction_mask=pred_mask_lesion,
-                                            reference_mask=true_mask_lesion)
+        tumor_burden = compute_tumor_burden(prediction_mask=submission_volume,
+                                            reference_mask=reference_volume)
         tumor_burden_list.append(tumor_burden)
         
         

program/helpers/calc_metric.py
@@ -65,8 +65,9 @@ def compute_tumor_burden(prediction_mask, reference_mask):
     def calc_tumor_burden(vol):
         num_liv_pix=np.count_nonzero(vol>=1)
         num_les_pix=np.count_nonzero(vol==2)
-        tumor_burden = num_les_pix/float(num_liv_pix)
-        return tumor_burden
+        if num_liv_pix:
+            return num_les_pix/float(num_liv_pix)
+        return np.inf
     tumor_burden_label= calc_tumor_burden(reference_mask)
     tumor_burden_pred = calc_tumor_burden(prediction_mask)
 