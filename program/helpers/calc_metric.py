Fix dim_sum sometimes having a broadcast error.

program/helpers/calc_metric.py
@@ -80,7 +80,7 @@ def detect_lesions(prediction_mask, reference_mask, min_overlap=0.5):
         specified axis, summing them together. Returns the reduced array.
         '''
         x = np.array(x)
-        if len(dims) <= 1:
+        if len(dims) < 2:
             return x
         
         # Initialize output
@@ -91,7 +91,7 @@ def detect_lesions(prediction_mask, reference_mask, min_overlap=0.5):
         # Sum over dims on axis
         sum_slices = [slice(None)]*x.ndim
         sum_slices[axis] = dims
-        dim_sum = np.sum(x[sum_slices], axis=axis)
+        dim_sum = np.sum(x[sum_slices], axis=axis, keepdims=True)
         
         # Remove all but first dim in dims
         mask = np.ones(x.shape, dtype=np.bool)